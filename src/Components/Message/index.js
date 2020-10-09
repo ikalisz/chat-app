8a44e0d8bd6a5defb6967fc757fad0401d3ffce3
Ian Kaliszewski <ikalisz@yahoo.com>
2020-10-08 21:43:58 -0600

    linting fixes

--- src/Components/Message/index.js
+++ src/Components/Message/index.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import styled from 'styled-components'
 
 function Message() {
   return (
+    <SContainer>
 
+    </SContainer>
   )
 }
 
 export default Message
+
+const SContainer = styled.div`
+
+`
