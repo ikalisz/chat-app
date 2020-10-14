dc1a2098829573d9b7121739de6c0147e9b76ae4
Ian Kaliszewski <ikalisz@yahoo.com>
2020-10-13 22:25:21 -0600

    added users map

--- src/Components/Users/index.js
+++ src/Components/Users/index.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import styled from 'styled-components'
 
-function Users() {
+function Users(users) {
+  const userComponent = users.map((ele) => (
+    <SContainer>
+      {ele}
+    </SContainer>
+  ))
+
   return (
     <SContainer>
-
+      {userComponent}
     </SContainer>
   )
 }
