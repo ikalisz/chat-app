94e0cc01368cbface94af3813ef04bdc7cc061bd
Ian Kaliszewski <ikalisz@yahoo.com>
2020-10-15 22:29:24 -0600

    added users and message to home

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import styled from 'styled-components'
+import Message from '../Message'
+import Users from '../Users'
 
 function Home() {
   return (
     <SContainer>
-      
+      <Users />
+      <Message />
     </SContainer>
   )
 }
