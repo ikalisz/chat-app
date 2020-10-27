6c1f81a6b18a7815fbddafb68fd6d77cb03d99eb
ikalisz <ikalisz@yahoo.com>
2020-10-26 22:14:18 -0600

    started usersetup

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -4,9 +4,16 @@ import Message from '../Message'
 import Users from '../Users'
 
 function Home() {
+  const [
+    selectedUser,
+    setSelectedUser,
+  ] = useState({})
   return (
     <SContainer>
-      <Users />
+      <Users
+        setSelectedUser={setSelectedUser}
+        selectedUser={selectedUser}
+      />
       <Message />
     </SContainer>
   )
